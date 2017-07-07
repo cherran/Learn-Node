d39b44eed41501494e37fe2a53f19dca88b9847f
cherran <cherranm@gmail.com>
2017-07-07 17:23:44 +0200

    Intro to routing

--- nodeweb/routes/index.js
+++ nodeweb/routes/index.js
@@ -1,9 +1,18 @@
 const express = require('express');
+
 const router = express.Router();
 
 // Do work here
+// (req, res) => {callback function}
 router.get('/', (req, res) => {
-  res.send('Hey! It works!');
+  res.json('Hey! It works!');
+});
+
+// getting parameters from the route
+router.get('/reverse/:name', (req, res) => {
+  // using those paremeters (ES6 sintax for the array?)
+  const reverse = [...req.params.name].reverse().join('');
+  res.send(reverse);
 });
 
 module.exports = router;
